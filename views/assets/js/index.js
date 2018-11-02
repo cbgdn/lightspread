b64098814bb99121a325dbe9f02d048e889736f3
Art4 <art4@wlabs.de>
2018-11-02 12:09:21 +0100

    add Bootstrap and node-sass

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import '../css/style.css';
 // import 'bootstrap/js/dist/collapse.js';
 // import 'bootstrap/scss/bootstrap.scss';
 // import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
 // import '@fortawesome/fontawesome-free/scss/solid.scss';
-console.log('Hello World!');
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.s?css$/,
                 use: [
                     {
                         loader: MiniCssExtractPlugin.loader,
