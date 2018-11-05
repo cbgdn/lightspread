aa1d08c71b11192d2e84b7d20e3c80eb0bc4b381
Art4 <art4@wlabs.de>
2018-11-05 14:22:04 +0100

    add  folder select UI

--- views/index.js
+++ views/index.js
@@ -2,21 +2,51 @@ const { dialog } = require('electron').remote;
 
 var selectedPath = null;
 
-document.querySelector('#folder-selector').addEventListener('click', (e) => {
-    var path = dialog.showOpenDialog({
-        properties: ['openDirectory']
+var selectFolder = function (selectedPath) {
+    document.querySelector('#path-selected').innerHTML = selectedPath;
+    document.querySelectorAll('.path-selected-indicator').forEach((el) => {
+        el.classList.remove('fa-circle');
+        el.classList.add('fa-check-circle');
+        el.classList.add('text-success');
     });
+};
 
-    if (path) {
-        selectedPath = path[0];
-        document.querySelector('#path-selected').innerHTML = selectedPath;
+var unselectFolder = function () {
+    document.querySelector('#path-selected').innerHTML = "<i>Kein Ordner gewählt</i>";
+    document.querySelectorAll('.path-selected-indicator').forEach((el) => {
+        el.classList.remove('text-success');
+        el.classList.remove('fa-check-circle');
+        el.classList.add('fa-circle');
+    });
+};
 
-        console.log(selectedPath);
+var unmuteServerSwitch = function () {
+    document.querySelector('.server-run-indicator').parentElement.classList.remove('text-muted');
+    document.querySelector('#server-switch').disabled = false;
+};
+
+var muteServerSwitch = function () {
+    document.querySelector('.server-run-indicator').parentElement.classList.add('text-muted');
+    document.querySelector('#server-switch').disabled = true;
+};
+
+var handleSelectedFolder = function (filePaths) {
+    if (filePaths) {
+        selectedPath = filePaths[0];
+        selectFolder(selectedPath);
+        unmuteServerSwitch();
     } else {
         selectedPath = null;
-        console.log('Kein Ordner gewählt');
-        document.querySelector('#path-selected').innerHTML = "<i>Kein Ordner gewählt</i>";
+        unselectFolder();
+        muteServerSwitch();
     }
+};
 
-
+document.querySelector('#folder-selector').addEventListener('click', (e) => {
+    dialog.showOpenDialog(
+        {
+            properties: ['openDirectory']
+        },
+        handleSelectedFolder
+    );
 });
