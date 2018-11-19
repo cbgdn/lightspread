cc61ddf7b2bd7ee44550fd219d62207752e893b8
Art4 <art4@wlabs.de>
2018-11-19 10:19:10 +0100

    Add button to start gallery
    refs #14

--- views/index.js
+++ views/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const { dialog } = require('electron').remote;
 const { ipcRenderer } = require('electron');
 const fs = require('fs');
@@ -7,17 +9,18 @@ const packageData = require('../package.json');
 const ImageStore = require('../src/imagestore');
 const store = new ImageStore();
 
-var selectedPath = null;
-var allowedExtensions = [
+let selectedPath = null;
+let allowedExtensions = [
     '.jpg',
     '.jpeg',
     '.png',
     '.gif',
 ];
-var server;
+let server;
+let serverHost = 'localhost';
 let serverPort = 8080;
 
-var selectFolder = (selectedPath) => {
+let selectFolder = (selectedPath) => {
     document.querySelector('#folder-selector').classList.add('btn-secondary');
     document.querySelector('#folder-selector').classList.remove('btn-primary');
     document.querySelector('#path-selected').innerHTML = selectedPath;
@@ -28,7 +31,7 @@ var selectFolder = (selectedPath) => {
     });
 };
 
-var unselectFolder = (msg) => {
+let unselectFolder = (msg) => {
     document.querySelector('#folder-selector').classList.add('btn-primary');
     document.querySelector('#folder-selector').classList.remove('btn-secondary');
     document.querySelector('#path-selected').innerHTML = "<i>"+msg+"</i>";
@@ -39,51 +42,53 @@ var unselectFolder = (msg) => {
     });
 };
 
-var unmuteServerSwitch = () => {
+let unmuteServerSwitch = () => {
     document.querySelector('.server-run-indicator').parentElement.classList.remove('text-muted');
     document.querySelector('#server-switch').disabled = false;
     document.querySelector('#server-switch').classList.add('btn-primary');
     document.querySelector('#server-switch').classList.remove('btn-secondary');
 };
 
-var muteServerSwitch = () => {
+let muteServerSwitch = () => {
     document.querySelector('.server-run-indicator').parentElement.classList.add('text-muted');
     document.querySelector('#server-switch').disabled = true;
     document.querySelector('#server-switch').classList.add('btn-secondary');
     document.querySelector('#server-switch').classList.remove('btn-primary');
 };
 
-var unmuteFolderSelector = () => {
+let unmuteFolderSelector = () => {
     document.querySelector('#folder-selector').disabled = false;
 };
 
-var muteFolderSelector = () => {
+let muteFolderSelector = () => {
     document.querySelector('#folder-selector').disabled = true;
 };
 
-var unmuteBrowserButton = () => {
-    document.querySelector('.browser-start-indicator').parentElement.classList.remove('text-muted');
+let unmuteBrowserButton = () => {
+    document.querySelector('.gallery-start-indicator').parentElement.classList.remove('text-muted');
+    document.querySelector('#gallery-start').disabled = false;
+    document.querySelector('#gallery-start').classList.add('btn-primary');
+    document.querySelector('#gallery-start').classList.remove('btn-secondary');
     document.querySelector('#browser-start').disabled = false;
-    document.querySelector('#browser-start').classList.add('btn-primary');
-    document.querySelector('#browser-start').classList.remove('btn-secondary');
 };
 
-var muteBrowserButton = () => {
-    document.querySelector('.browser-start-indicator').parentElement.classList.add('text-muted');
+let muteBrowserButton = () => {
+    document.querySelector('.gallery-start-indicator').parentElement.classList.add('text-muted');
+    document.querySelector('#gallery-start').disabled = true;
+    document.querySelector('#gallery-start').classList.add('btn-secondary');
+    document.querySelector('#gallery-start').classList.remove('btn-primary');
     document.querySelector('#browser-start').disabled = true;
-    document.querySelector('#browser-start').classList.add('btn-secondary');
-    document.querySelector('#browser-start').classList.remove('btn-primary');
 };
 
-var startServer = () => {
-    var app = express();
+let startServer = () => {
+    let app = express();
 
     app.get(['/', '/index.html'], function (req, res) {
         res.sendFile(path.resolve(__dirname, './gallery.html'));
     });
 
     app.get(['/dist/:name'], function (req, res) {
-        var name = req.params.name;
+        let name = req.params.name;
 
         fs.access(path.resolve(__dirname, './dist/' + name), fs.constants.F_OK, (err) => {
             if (err) {
@@ -117,7 +122,7 @@ var startServer = () => {
     });
 
     app.get(['/images/:name'], function (req, res) {
-        var name = req.params.name;
+        let name = req.params.name;
 
         store.getData(name)
         .catch(() => {
@@ -129,7 +134,7 @@ var startServer = () => {
     });
 
     app.get(['/thumbs/:name'], function (req, res) {
-        var name = req.params.name;
+        let name = req.params.name;
 
         store.getData(name)
         .catch(() => {
@@ -147,7 +152,7 @@ var startServer = () => {
     console.log('Server gestartet');
 };
 
-var stopServer = () => {
+let stopServer = () => {
     if (server) {
         server.close();
         server = null;
@@ -158,7 +163,11 @@ var stopServer = () => {
     console.log('Server gestoppt');
 };
 
-var handleSelectedFolder = (filePaths) => {
+let startGallery = (url) => {
+    ipcRenderer.send('openexternalpage', url);
+};
+
+let handleSelectedFolder = (filePaths) => {
     if (! filePaths) {
         selectedPath = null;
         store.reset();
@@ -187,13 +196,13 @@ var handleSelectedFolder = (filePaths) => {
 
         files.forEach((value, index) => {
             // Ignore files/folders with wrong extensions
-            var ext = path.extname(value).toLowerCase();
+            let ext = path.extname(value).toLowerCase();
             if (! allowedExtensions.includes(ext)) {
                 return;
             }
 
             // ignore folders and symlinks
-            var stat = fs.statSync(selectedPath + value);
+            let stat = fs.statSync(selectedPath + value);
             if (! stat.isFile()) {
                 return;
             }
@@ -212,6 +221,7 @@ var handleSelectedFolder = (filePaths) => {
     });
 };
 
+// If folder selector is clicked
 document.querySelector('#folder-selector').addEventListener('click', (e) => {
     dialog.showOpenDialog(
         {
@@ -221,6 +231,7 @@ document.querySelector('#folder-selector').addEventListener('click', (e) => {
     );
 });
 
+// If server switch is clicked
 document.querySelector('#server-switch').addEventListener('click', (e) => {
     if (server) {
         stopServer();
@@ -238,7 +249,7 @@ document.querySelector('#server-switch').addEventListener('click', (e) => {
         startServer();
         unmuteBrowserButton();
         e.target.innerHTML = 'Server stoppen';
-        document.querySelector('#server-status').innerHTML = '<i>Server gestartet unter http://localhost:'+serverPort+'</i>';
+        document.querySelector('#server-status').innerHTML = '<i>Server gestartet unter http://'+serverHost+':'+serverPort+'</i>';
         document.querySelectorAll('.server-run-indicator').forEach((el) => {
             el.classList.remove('fa-circle');
             el.classList.add('fa-check-circle');
@@ -247,13 +258,25 @@ document.querySelector('#server-switch').addEventListener('click', (e) => {
     }
 });
 
-// open links in external browser
-var externalButtons = document.getElementsByClassName('open-external');
+// If gallery starter is clicked
+document.querySelector('#gallery-start').addEventListener('click', (e) => {
+    e.preventDefault();
+    startGallery('http://'+serverHost+':'+serverPort);
+});
 
-for (var i = 0; i < externalButtons.length; i++) {
+// If browser starter is clicked
+document.querySelector('#browser-start').addEventListener('click', (e) => {
+    e.preventDefault();
+    ipcRenderer.send('openexternalpage', 'http://'+serverHost+':'+serverPort);
+});
+
+// open links in external browser
+let externalButtons = document.getElementsByClassName('open-external');
+
+for (let i = 0; i < externalButtons.length; i++) {
     externalButtons[i].addEventListener('click', function(e) {
         e.preventDefault();
-        var target = '';
+        let target = '';
 
         if (this.href) {
             target = this.href;
@@ -271,7 +294,7 @@ ipcRenderer.on('app-shutdown', (event, arg) => {
 })
 
 // Set app version
-var elements = document.getElementsByClassName('ls-version');
+let elements = document.getElementsByClassName('ls-version');
 
 for (const element of elements) {
     element.innerHTML = packageData.name + ' ' + packageData.version;
